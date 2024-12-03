bac23d036e32c0e4df308c0a8c41956982ccb14f
Verunchik27062003 <vpozniak@st.swps.edu.pl>
2024-12-03 11:30:56 +0100

    new

--- myproject/myproject/myapp/admin.py
+++ myproject/myproject/myapp/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Team, Person
+from .models import Team, Person, Osoba, Stanowisko
 
 admin.site.register(Team)
 admin.site.register(Person)
+admin.site.register(Stanowisko)
+class OsobaAdmin (admin.ModelAdmin):
+    list_display = ["imie", "nazwisko", "plec", "stanowisko", "data_dodania"]
+admin.site.register(Osoba, OsobaAdmin)
